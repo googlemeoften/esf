8ac484225d3fbd369afb6f8b5879b612c3bb6890
Dave <1919435320@qq.com>
2016-12-15 15:08:06 +0800

    UUID生成工具类

---
+++ esf-common/src/main/java/cn/edu/esf/utils/UUIDGenerator.java
@@ -0,0 +1,18 @@
+package cn.edu.esf.utils;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 生成唯一UUID
+ *
+ * @Author heyong
+ * @Date 2016/12/14
+ */
+public class UUIDGenerator {
+    private static AtomicLong opaque = new AtomicLong();
+
+    public static final synchronized long getNextOpaque() {
+        return opaque.getAndDecrement();
+    }
+
+}
