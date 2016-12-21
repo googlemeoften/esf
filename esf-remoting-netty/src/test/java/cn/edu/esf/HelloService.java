dbc14bc0b75443e7ff15216e22958e6059e0aadb
Dave <1919435320@qq.com>
2016-12-21 11:58:52 +0800

    测试

---
+++ esf-remoting-netty/src/test/java/cn/edu/esf/HelloService.java
@@ -0,0 +1,9 @@
+package cn.edu.esf;
+
+/**
+ * @Author heyong
+ * @Date 2016/12/20
+ */
+public interface HelloService {
+    public String sayHello(String str);
+}
--- esf-remoting-netty/src/test/java/cn/edu/esf/client/ClientTest.java
+++ esf-remoting-netty/src/test/java/cn/edu/esf/client/ClientTest.java
@@ -1,14 +1,28 @@
 package cn.edu.esf.client;
 
 
+import cn.edu.esf.HelloService;
+import cn.edu.esf.RemotingConstants;
+import cn.edu.esf.RpcRequest;
+
+
 public class ClientTest {
 
     public static void main(String[] args) throws Exception {
         NettyClientFactory factory = NettyClientFactory.getInstance();
         NettyClient client = (NettyClient) factory.createClient("127.0.0.1");
-        client.sendRequest("hello", 4000);
 
-       // Thread.sleep(100000);
+        Class clazz = HelloService.class;
+        String targetInstance = clazz.getName();
+        String methodName = clazz.getMethod("sayHello", String.class).getName();
+        String[] argsTypes = {String.class.getName()};
+        byte[][] requestObject = new byte[1][];
+        byte[] bytes = "hello".getBytes(RemotingConstants.DEFAULT_CHARSET);
+        requestObject[0] = bytes;
+
+        RpcRequest request = new RpcRequest(100L, 2000, targetInstance, methodName, argsTypes, requestObject, "hello".getBytes(), 0);
+        client.sendRequest(request, 2000);
+
     }
 
 }
