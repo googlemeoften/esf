8a0326531602ef2bb84af47b69d4476adfe1165c
googlemeoften <1919435320@qq.com>
2017-02-13 10:57:44 +0800

    限流服务接口注解添加

--- esf-services/src/main/java/cn/edu/esf/tps/TPSLimitService.java
+++ esf-services/src/main/java/cn/edu/esf/tps/TPSLimitService.java
@@ -8,8 +8,16 @@ import cn.edu.esf.domain.ESFRequest;
  * @Date 2017/1/4
  */
 public interface TPSLimitService {
-
+    /**
+     * 限流服务是否有效
+     * @return
+     */
     boolean isValid();
 
+    /**
+     * 处理ESF请求，判断是否响应服务
+     * @param hsfRequest
+     * @return
+     */
     TPSResult process(ESFRequest hsfRequest);
 }
