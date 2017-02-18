e02e6051d76b857e72d2ee797c65675b8cc773e9
googlemeoften <1919435320@qq.com>
2017-02-18 22:13:50 +0800

    协议处理接口

--- esf-remoting/src/main/java/cn/edu/esf/server/ProtocolHandler.java
+++ esf-remoting/src/main/java/cn/edu/esf/server/ProtocolHandler.java
@@ -12,7 +12,18 @@ import java.util.concurrent.Executor;
  * @Date 2016/12/15
  */
 public interface ProtocolHandler<T extends BaseRequest> {
+    /**
+     * 业务线程逻辑入口
+     * @param request
+     * @param connection
+     * @param startTime
+     */
     public void handleRequest(final T request, final Connection connection, final long startTime);
 
+    /**
+     * 根据请求获取业务线程池
+     * @param request
+     * @return
+     */
     public Executor getExecutor(final T request);
 }
