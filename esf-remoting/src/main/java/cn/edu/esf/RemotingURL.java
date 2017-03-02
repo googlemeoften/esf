5c794707f2b3126e1e4bef6399cd6472e699f056
googlemeoften <1919435320@qq.com>
2017-03-02 18:22:04 +0800

    RemotingURL修改

--- esf-remoting/src/main/java/cn/edu/esf/RemotingURL.java
+++ esf-remoting/src/main/java/cn/edu/esf/RemotingURL.java
@@ -144,4 +144,29 @@ public class RemotingURL {
         return parameters.get(key);
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((url == null) ? 0 : url.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        RemotingURL other = (RemotingURL) obj;
+        if (url == null) {
+            if (other.url != null)
+                return false;
+        } else if (!url.equals(other.url))
+            return false;
+        return true;
+    }
+
 }
