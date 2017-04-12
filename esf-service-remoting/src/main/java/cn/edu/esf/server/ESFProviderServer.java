909969d536711435960ecece8af02ac0c28336e5
1919435320@qq.com <a941213a>
2017-04-12 10:12:54 +0800

    ESF服务端启动

--- esf-service-remoting/src/main/java/cn/edu/esf/server/ESFProviderServer.java
+++ esf-service-remoting/src/main/java/cn/edu/esf/server/ESFProviderServer.java
@@ -17,13 +17,14 @@ import org.slf4j.LoggerFactory;
  * @author heyong
  * @Date 2017/2/13
  */
-public class ESFProviderServer implements ProviderServer{
+public class ESFProviderServer implements ProviderServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(ESFProviderServer.class);
     private static final String LINESEPARATOR = System.getProperty("line.separator");
+    private static final String LINE = "------------------------------------";
 
-    private final ConfigurationService configurationService = ESFServiceContainer.getInstance(ConfigurationService.class);
+    private final ConfigurationService configService = ESFServiceContainer.getInstance(ConfigurationService.class);
     private final RpcRequestProcessor processor = ESFServiceContainer.getInstance(RpcRequestProcessor.class);
-    private final Server server = new NettyServer(processor, configurationService.getBindAddress());
+    private final Server server = new NettyServer(processor, configService.getBindAddress());
     private final ThreadPoolManager manager = processor.getThreadPoolManager();
 
 
@@ -34,21 +35,35 @@ public class ESFProviderServer implements ProviderServer{
 
     @Override
     public void allocThreadPool(String uniqueName, int corePoolSize, int maxPoolSize) throws ESFException {
-
+        manager.allocThreadPool(uniqueName, corePoolSize, maxPoolSize);
     }
 
     @Override
     public void startHSFServer() throws ESFException {
+        try {
+            server.start(configService.getESFServerPort());
+        } catch (Exception e) {
+            throw new ESFException("ESF 服务端启动失败");
+        }
 
+        StringBuilder sb = new StringBuilder();
+        sb.append(LINE).append(LINESEPARATOR);
+        sb.append(" HSF服务器已启动: " + configService.getESFServerPort()).append(LINESEPARATOR);
+        sb.append(LINE).append(LINESEPARATOR);
+        sb.append(" I/O线程数     : " + (Runtime.getRuntime().availableProcessors() + 1)).append(LINESEPARATOR);
+        sb.append(" 处理最小线程数 : " + configService.getESFServerMinPoolSize()).append(LINESEPARATOR);
+        sb.append(" 处理最大线程数 : " + configService.getESFServerMaxPoolSize()).append(LINESEPARATOR);
+        sb.append(LINE);
+        LOGGER.warn(sb.toString());
     }
 
     @Override
     public void stopHSFServer() throws ESFException {
-
+        server.stop();
     }
 
-    @Override
-    public void refuseConnect() {
-
-    }
+//    @Override
+//    public void refuseConnect() {
+//
+//    }
 }
